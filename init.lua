7a6e99eac97cf464717318f64095e4effad8ca95
Alex Solovev <alex.solovev.work@gmail.com>
2025-01-18 22:48:14 +0800

    add some config options from kickstart

--- init.lua
+++ init.lua
@@ -2,16 +2,6 @@
 vim.hl = vim.highlight
 
 require('config.lazy')
-require('oil').setup({
-  columns = {
-    'icon',
-    'permissions',
-    -- 'size',
-    -- 'mtime',
-  },
-})
-
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- Basic formatting and line numbers
 vim.opt.shiftwidth = 4
@@ -19,4 +9,26 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable paste from clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+-- Enable Nerd font
+vim.g.have_nerd_font = true
+-- Save undo history
+vim.opt.undofile = true
+-- Case insensitive search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Decrease update time
+vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 30
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,11 +1,22 @@
 return {
   {
     'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
     -- Optional dependencies
-    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+    config = function()
+      require('oil').setup({
+        columns = {
+          'icon',
+          -- 'permissions',
+          -- 'size',
+          -- 'mtime',
+        }
+      })
+
+      vim.keymap.set('n', '-', function()
+        require('oil').open()
+      end, { desc = 'Open parent directory' })
+    end
   }
 }
