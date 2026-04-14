ff00e4c8b9094553bd6eb021eb61484f3e1640a5
Alex Solovev <alex.solovev.work@gmail.com>
2026-04-14 21:52:11 +0800

    basics & custo statusline

--- init.lua
+++ init.lua
@@ -1,46 +1,119 @@
 -- Fix for https://github.com/neovim/neovim/issues/31675
 vim.hl = vim.highlight
-require("config.lazy")
 
--- Basic formatting and line numbers
-vim.opt.shiftwidth = 4
+vim.g.mapleader = " "
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+
+vim.opt.ignorecase = true 
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.shiftwidth = 4 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.signcolumn = "yes:1"
+vim.opt.showmatch = true
+vim.opt.cmdheight = 0
+vim.opt.pumheight = 10 
+vim.opt.pumblend = 10 
+vim.opt.winblend = 10 
+vim.opt.conceallevel = 0 
+vim.opt.concealcursor = "" 
+vim.opt.lazyredraw = true
+vim.opt.synmaxcol = 300
+vim.opt.fillchars = { eob = " " }
+
+vim.opt.undofile = true 
+vim.opt.showmode = false
+vim.opt.confirm = true
+vim.opt.winborder = "rounded"
+
+vim.g.have_nerd_font = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Folding (requires treesitter)
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+
+-- Undo dir config
+local undodir = vim.fn.expand("~/.vim/undodir")
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = undodir
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.redrawtime = 10000
+vim.opt.maxmempattern = 20000
+vim.opt.autoread = true -- audo reload files changed outside of nvim
+vim.opt.autowrite = false
+vim.opt.hidden = true -- allow hidden buffers
+vim.opt.errorbells = false
+vim.opt.backspace = "indent,eol,start"
+vim.opt.autochdir = false
+vim.opt.iskeyword:append("-")
+vim.opt.path:append("**")
+vim.opt.selection = "inclusive"
+vim.opt.mouse = "a"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.modifiable = true
+vim.opt.encoding = "utf-8"
+
+-- Install Packages
+vim.pack.add {
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/oskarnurm/koda.nvim",
+  "https://github.com/saghen/blink.cmp",
+}
+
+require("oil").setup()
+require("blink.cmp").setup({
+  fuzzy = { implementation = "prefer_rust_with_warning" }
+})
+
+vim.cmd.colorscheme("koda")
+vim.cmd("syntax off")
+
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("lua_ls")
+
 
 -- Enable paste from clipboard
 vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
 end)
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
--- Enable Nerd font
-vim.g.have_nerd_font = true
--- Save undo history
-vim.opt.undofile = true
--- Case insensitive search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
--- Decrease update time
-vim.opt.updatetime = 250
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
--- Show which line your cursor is on
-vim.opt.cursorline = true
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Show hover documentation with rounded border
-vim.keymap.set("n", "<S-k>", function()
-  vim.lsp.buf.hover({ border = "single" })
-end, { desc = "" })
+vim.keymap.set("n", "<S-k>", vim.lsp.buf.hover, { desc = "" })
 
 -- Highlight on copy
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
@@ -51,3 +124,150 @@ vim.keymap.set("n", "<leader>td", function()
   local config = vim.diagnostic.config().virtual_lines
   vim.diagnostic.config({ virtual_lines = not config })
 end, { desc = "Toggle diagnostics" })
+
+-- Open Oil
+vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open Oil" })
+-- Go to definition
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+
+-- Git branch function with caching and Nerd Font icon
+local cached_branch = ""
+local last_check = 0
+local function git_branch()
+	local now = vim.loop.now()
+	if now - last_check > 5000 then -- Check every 5 seconds
+		cached_branch = vim.fn.system("git branch --show-current 2>/dev/null | tr -d '\n'")
+		last_check = now
+	end
+	if cached_branch ~= "" then
+		return " \u{e725} " .. cached_branch .. " " -- nf-dev-git_branch
+	end
+	return ""
+end
+
+-- File type with Nerd Font icon
+local function file_type()
+	local ft = vim.bo.filetype
+	local icons = {
+		lua = "\u{e620} ", -- nf-dev-lua
+		python = "\u{e73c} ", -- nf-dev-python
+		javascript = "\u{e74e} ", -- nf-dev-javascript
+		typescript = "\u{e628} ", -- nf-dev-typescript
+		javascriptreact = "\u{e7ba} ",
+		typescriptreact = "\u{e7ba} ",
+		html = "\u{e736} ", -- nf-dev-html5
+		css = "\u{e749} ", -- nf-dev-css3
+		scss = "\u{e749} ",
+		json = "\u{e60b} ", -- nf-dev-json
+		markdown = "\u{e73e} ", -- nf-dev-markdown
+		vim = "\u{e62b} ", -- nf-dev-vim
+		sh = "\u{f489} ", -- nf-oct-terminal
+		bash = "\u{f489} ",
+		zsh = "\u{f489} ",
+		rust = "\u{e7a8} ", -- nf-dev-rust
+		go = "\u{e724} ", -- nf-dev-go
+		c = "\u{e61e} ", -- nf-dev-c
+		cpp = "\u{e61d} ", -- nf-dev-cplusplus
+		java = "\u{e738} ", -- nf-dev-java
+		php = "\u{e73d} ", -- nf-dev-php
+		ruby = "\u{e739} ", -- nf-dev-ruby
+		swift = "\u{e755} ", -- nf-dev-swift
+		kotlin = "\u{e634} ",
+		dart = "\u{e798} ",
+		elixir = "\u{e62d} ",
+		haskell = "\u{e777} ",
+		sql = "\u{e706} ",
+		yaml = "\u{f481} ",
+		toml = "\u{e615} ",
+		xml = "\u{f05c} ",
+		dockerfile = "\u{f308} ", -- nf-linux-docker
+		gitcommit = "\u{f418} ", -- nf-oct-git_commit
+		gitconfig = "\u{f1d3} ", -- nf-fa-git
+		vue = "\u{fd42} ", -- nf-md-vuejs
+		svelte = "\u{e697} ",
+		astro = "\u{e628} ",
+	}
+
+	if ft == "" then
+		return " \u{f15b} " -- nf-fa-file_o
+	end
+
+	return ((icons[ft] or " \u{f15b} ") .. ft)
+end
+
+-- File size with Nerd Font icon
+local function file_size()
+	local size = vim.fn.getfsize(vim.fn.expand("%"))
+	if size < 0 then
+		return ""
+	end
+	local size_str
+	if size < 1024 then
+		size_str = size .. "B"
+	elseif size < 1024 * 1024 then
+		size_str = string.format("%.1fK", size / 1024)
+	else
+		size_str = string.format("%.1fM", size / 1024 / 1024)
+	end
+	return " \u{f016} " .. size_str .. " " -- nf-fa-file_o
+end
+
+-- Mode indicators with Nerd Font icons
+local function mode_icon()
+	local mode = vim.fn.mode()
+	local modes = {
+		n = " \u{f121}  NORMAL",
+		i = " \u{f11c}  INSERT",
+		v = " \u{f0168} VISUAL",
+		V = " \u{f0168} V-LINE",
+		["\22"] = " \u{f0168} V-BLOCK",
+		c = " \u{f120} COMMAND",
+		s = " \u{f0c5} SELECT",
+		S = " \u{f0c5} S-LINE",
+		["\19"] = " \u{f0c5} S-BLOCK",
+		R = " \u{f044} REPLACE",
+		r = " \u{f044} REPLACE",
+		["!"] = " \u{f489} SHELL",
+		t = " \u{f120} TERMINAL",
+	}
+	return modes[mode] or (" \u{f059} " .. mode)
+end
+
+_G.mode_icon = mode_icon
+_G.git_branch = git_branch
+_G.file_type = file_type
+_G.file_size = file_size
+
+vim.cmd([[
+  highlight StatusLineBold gui=bold cterm=bold
+]])
+
+local function setup_dynamic_statusline()
+	vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+		callback = function()
+			vim.opt_local.statusline = table.concat({
+				"  ",
+				"%#StatusLineBold#",
+				"%{v:lua.mode_icon()}",
+				"%#StatusLine#",
+				" \u{e0b1} %f %h%m%r", -- nf-pl-left_hard_divider
+				"%{v:lua.git_branch()}",
+				"\u{e0b1} ", -- nf-pl-left_hard_divider
+				"%{v:lua.file_type()}",
+				"\u{e0b1} ", -- nf-pl-left_hard_divider
+				"%{v:lua.file_size()}",
+				"%=", -- Right-align everything after this
+				" \u{f017} %l:%c  %P ", -- nf-fa-clock_o for line/col
+			})
+		end,
+	})
+	vim.api.nvim_set_hl(0, "StatusLineBold", { bold = true })
+
+	vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+		callback = function()
+			vim.opt_local.statusline = "  %f %h%m%r \u{e0b1} %{v:lua.file_type()} %=  %l:%c   %P "
+		end,
+	})
+end
+
+setup_dynamic_statusline()
